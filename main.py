36d25caf4dd1b9a815c782641fea7f6be18a7d1b
ItsMeAbby <malik_muhammad222@outlook.com>
2023-02-13 19:29:26 +0500

    updated code

--- main.py
+++ main.py
@@ -7,54 +7,30 @@ from fastapi import FastAPI, Body, Request, Form
 from fastapi import FastAPI, File, UploadFile
 from newpaddle import OCR, parsing
 from paddleocr import PaddleOCR
-from newpaddle import pdfconverter
-from newpaddle import empty_directory
+import io
+import numpy as np
+from PIL import Image
 
 app = FastAPI()
-ocr = PaddleOCR(use_angle_cls=True, lang='en')
+ocr = PaddleOCR(use_angle_cls=True, lang='en',show_log = False)
 
-
-templates = Jinja2Templates(directory="htmldirectory")
 @app.get("/")
 async def root():
-    return FileResponse('./htmldirectory/index.html')
+    return {"message": "Server is up and running!"}
 
 @app.post("/scan")
 async def scan_image(request:Request, file:UploadFile):
-    folder = "image-upload"
-    if not os.path.isdir(f"./{folder}"):
-        os.mkdir(f"./{folder}")
-        
-    file_name=str(file.filename)
-    with open(f"./{folder}/{file_name}", "wb+") as file_object:
-        file_object.write(file.file.read())
-    file_path=f"./{folder}/{file_name}"
-    if file_name.split(".")[1] == "pdf":
-        pdfconverter(file_path, folder)
-        os.remove(file_path)
-        for image in os.listdir(f"./{folder}"):
-            file_path = f'{folder}/{image}'
-
-    try:
-        list_of_words = OCR(file_path, ocr)
+    
+    # try:
+        image_bytes = file.file.read()
+        image = np.array(Image.open(io.BytesIO(image_bytes))) 
+        list_of_words = OCR(image, ocr)
         final_results = parsing(list_of_words)
-
-        html_content = f"""
-                        <html>
-                            <head>
-                                <title>Results</title>
-                            </head>
-                            <body>
-                                <h1>"{"Scanning Image"}"</h1>
-                            </body>
-                        </html>
-                        """
-        
-        
-            # return FileResponse('./htmldirectory/df_representation.html')
-        empty_directory("image_upload")
+        # return FileResponse('./htmldirectory/df_representation.html')
+        # empty_directory("image_upload")
         return final_results
-    except:
-        empty_directory("image_upload")
-        return "INVALID IMAGE! TRY AGAIN PLEASE."
+    # except Exception as e:
+    #     print(e)
+    #     # empty_directory("image_upload")
+    #     return {"error": str(e)}
 
--- newpaddle.py
+++ newpaddle.py
@@ -1,10 +1,10 @@
-from paddleocr import PaddleOCR,draw_ocr
+# from paddleocr import PaddleOCR
 import re 
-import cv2
+# import cv2
 import datetime
-import fitz
-import os
-import glob
+# # import fitz
+# import os
+# import glob
 
 
 # Paddleocr supports Chinese, English, French, German, Korean and Japanese.
@@ -17,19 +17,18 @@ def OCR(img_path, ocr):
     for idx in range(len(result)):
         res = result[idx]
         for line in res:
-            # print(line[1][0])
             words.append(line[1][0])
     
     return words
 
-def pdfconverter(fname, folder):
-    dpi = 300  # choose desired dpi here
-    zoom = dpi / 72  # zoom factor, standard: 72 dpi
-    magnify = fitz.Matrix(zoom, zoom)  # magnifies in x, resp. y direction
-    doc = fitz.open(fname)  # open document
-    for page in doc:
-        pix = page.get_pixmap(matrix=magnify)  # render page to an image
-        pix.save(f"{folder}/page-{page.number}.png")
+# def pdfconverter(fname, folder):
+#     dpi = 300  # choose desired dpi here
+#     zoom = dpi / 72  # zoom factor, standard: 72 dpi
+#     magnify = fitz.Matrix(zoom, zoom)  # magnifies in x, resp. y direction
+#     doc = fitz.open(fname)  # open document
+#     for page in doc:
+#         pix = page.get_pixmap(matrix=magnify)  # render page to an image
+#         pix.save(f"{folder}/page-{page.number}.png")
 
 def parsing(words_list):
     for word in words_list:
@@ -67,14 +66,14 @@ def parsing(words_list):
     }
     return response_dict
 
-def empty_directory(directory):
-    files = glob.glob(f'{directory}/*')
-    for f in files:
-        os.remove(f)
+# def empty_directory(directory):
+#     files = glob.glob(f'{directory}/*')
+#     for f in files:
+#         os.remove(f)
 
-if __name__ == "__main__":
-    ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
-    img_path = "images/IMG_6637.jpg"
-    list_of_words = OCR(img_path, ocr)
-    final_results = parsing(list_of_words)
-    print(final_results)
+# if __name__ == "__main__":
+#     ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
+#     img_path = "images/IMG_6637.jpg"
+#     list_of_words = OCR(img_path, ocr)
+#     final_results = parsing(list_of_words)
+#     print(final_results)
